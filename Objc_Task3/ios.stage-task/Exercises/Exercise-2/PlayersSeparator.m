5d05443b8013bc3c5d5f5463e4bdf4292fc86c34
Alisher <alishktl99@gmail.com>
2021-05-24 23:24:12 +0600

    done with first 3 tasks

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,35 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int ans = 0;
+    BOOL ratingIlessThanJ;
+    BOOL ratingJLessThanK;
+    BOOL sortedAscending;
+    BOOL ratingIGreaterThanJ;
+    BOOL ratingJGreaterThanK;
+    BOOL sortedDescending;
+    
+    for (int i = 0; i < ratingArray.count; ++i) {
+        
+        for (int j = i + 1; j < ratingArray.count; ++j) {
+            
+            for (int k = j + 1; k < ratingArray.count; ++k) {
+                
+                ratingIGreaterThanJ = [ratingArray[i] intValue] > [ratingArray[j] intValue];
+                ratingJGreaterThanK = [ratingArray[j] intValue] > [ratingArray[k] intValue];
+                sortedDescending = ratingIGreaterThanJ && ratingJGreaterThanK;
+                
+                ratingIlessThanJ = [ratingArray[i] intValue] < [ratingArray[j] intValue];
+                ratingJLessThanK = [ratingArray[j] intValue] < [ratingArray[k] intValue];
+                sortedAscending = ratingIlessThanJ && ratingJLessThanK;
+                
+                if (sortedDescending || sortedAscending){
+                    ans++;
+                }
+            }
+        }
+    }
+    return ans;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,29 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *str1 = [NSMutableString stringWithString:string1];
+    NSMutableString *str2 = [NSMutableString stringWithString:string2];
+    
+    NSMutableString *ans = [NSMutableString stringWithString:@""];
+    
+    while(str1.length != 0 || str2.length != 0) {
+        if (str1.length == 0) {
+            [ans appendString:str2];
+            break;
+        }
+        if (str2.length == 0) {
+            [ans appendString:str1];
+            break;
+        }
+        if ([[str1 substringToIndex:1] UTF8String][0] <= [[str2 substringToIndex:1] UTF8String][0]) {
+            [ans appendString: [str1 substringToIndex:1]];
+            str1 = [[str1 substringFromIndex:1] mutableCopy];
+        } else {
+            [ans appendString: [str2 substringToIndex:1]];
+            str2 = [[str2 substringFromIndex:1] mutableCopy];
+        }
+    }
+    return ans;
 }
 
 @end
